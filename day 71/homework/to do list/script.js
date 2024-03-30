deec629eb3c2a8bd2fa0a07fb681c21954a25821
sosokereselidze <soso.kereselidze1@gmail.com>
2024-03-30 20:38:00 +0400

    idk

--- day 71/homework/to do list/script.js
+++ day 71/homework/to do list/script.js
@@ -1,21 +1,16 @@
 function addTask() {
   var taskInput = document.getElementById('taskInput');
   var taskList = document.getElementById('taskList');
-
   var taskText = taskInput.value;
-
   if (taskText.trim() === '') {
       alert('Please enter a task.');
       return;
   }
-
   var listItem = document.createElement('li');
-
   listItem.innerHTML = '<input class="check" type="checkbox" onclick="checkTask(this)">' + taskText +
                        '<button class="deleteButton" onclick="deleteTask(this)">Delete</button>';
 
   taskList.appendChild(listItem);
-
   taskInput.value = '';
 }
 
@@ -23,7 +18,6 @@ function checkTask(checkbox) {
   var listItem = checkbox.parentNode;
   listItem.classList.toggle('checked', checkbox.checked);
 }
-
 
 function deleteTask(button) {
   var listItem = button.parentNode;
